9abad5574da2dbc04ebc0501a1ce62cde92c144b
vince123-create <vincentbertacchi@icloud.com>
2026-03-19 18:37:14 -0400

    fix static path

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const SCHEDULE_FILE = './schedule.json';
 
