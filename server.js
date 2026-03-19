88b7ad6f70ffe32a4a9e2231e9ef964eb7357934
vince123-create <vincentbertacchi@icloud.com>
2026-03-19 19:10:05 -0400

    fix public path v2

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+});
+
 const SCHEDULE_FILE = './schedule.json';
 
 function loadSchedule() {
